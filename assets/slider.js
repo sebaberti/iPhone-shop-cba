54ebd9720dcd47e045e2a729c821b1aaba36b3b3
sebastian <sebaberti.8@gmail.com>
2022-11-18 16:30:14 -0300

    se cambio el slider

---
+++ assets/slider.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.mySwiper', {
+	spaceBetween:0 ,
+	centeredSlides: true,
+	autoplay: {
+		delay: 3500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+       
+	},
+   
+});
+
+
+
+
